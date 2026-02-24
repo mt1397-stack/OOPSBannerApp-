961ec4d25deb66b8431d7e72bab8819cd6868736
Manimala <mt1397@srmist.edu.in>
2026-02-24 10:50:16 +0530

    ADDED UC4

---
+++ OOPSBannerApp.java
@@ -0,0 +1,13 @@
+public class OOPSBannerApp{
+   public static void main(String[] args){
+    lines[0] = System.out.println(String.join(" ", "   ***   ","   ***    ","  ******    ","   ***** "));
+    lines[1] = System.out.printlnstring.join(" "," **    ** "," **    ** ","  **     ** ","  **     "));
+    lines[2] = System.out.printlnstring.join(" "," **    ** "," **    ** ","  **      **","**       "));
+    lines[3] = System.out.print.lnstring.join(" ","**    ** ","**     ** ","  **     ** ","   ***** "));
+    lines[4] = System.out.print.lnstring.join(" ","**    ** ","**     ** ","  ******    ","       **"));
+    lines[5] = System.out.print.lnstring.join(" ",  **   ** ","  **   ** ","  **        "," **    **"));
+    lines[6] = System.out.print.lnstring.join(" ",    ***   ","    ***   ","  **        ","   ***** "));
+	for (String line : lines) {
+	     System.out.println(line);
+    }
+}	
