010c9f87d5ad0b7ed790326e3cb1b88edc226dff
Manimala <mt1397@srmist.edu.in>
2026-03-03 10:03:23 +0530

    console UC4 to print OOPS

---
+++ OOPSBannerApp5.java
@@ -0,0 +1,22 @@
+public class OOPSBannerApp4 {
+
+    public static void main(String[] args) {
+
+    
+        String[] lines = {
+
+    
+        String.join("", "      ***      ***      *****       ***** ");
+        String.join("", "     ** **    ** **    **   **    **      ");
+        String.join("", "    **   **  **   **   **   **   **       ");
+        String.join("", "    **   **  **   **   *****      ***     ");
+        String.join("", "    **   **  **   **   **            **   ");
+        String.join("", "     ** **    ** **    **             **  ");
+        String.join("", "      ***      ***     **        *****    ");
+
+        
+        for (String line : lines) {
+            System.out.println(line);
+        }
+    }
+}
