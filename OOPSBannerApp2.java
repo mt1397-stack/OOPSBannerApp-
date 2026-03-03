0691702da267e45633e8acff3a3ce5d11fc6a453
Manimala <mt1397@srmist.edu.in>
2026-03-03 22:04:52 +0530

    Added OOPSBannerApp2 file

---
+++ OOPSBannerApp2.java
@@ -0,0 +1,18 @@
+/**
+ * OOPSBannerApp UC2 - Banner Display Application
+ * Displays the word "OOPS" using ASCII art.
+ */
+
+public class OOPSBannerApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("  ***     ***    ******    ***** ");
+        System.out.println(" **  **   **  **  **   **   **   **");
+        System.out.println(" **  **   **  **  **   **   **   **");
+        System.out.println(" **  **   **  **  ******    ***** ");
+        System.out.println(" **  **   **  **  **        **  **");
+        System.out.println(" **  **   **  **  **        **   **");
+        System.out.println("  ***     ***    **        ***** ");
+    }
+}
